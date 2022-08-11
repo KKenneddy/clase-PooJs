189ef7308ae66b7dc5bba11e33e995d55cb80955
Kennedy <nofuequequise@gmail.com>
2022-08-11 14:30:14 -0400

    Cap12

---
+++ src/cap12/main.mjs
@@ -0,0 +1,25 @@
+function videoPlay(id) {
+  const urlSecreta = "https://platzimasseguroquelanasa.com";
+  console.log("Se esta reproduciendo desde la url" + urlSecreta);
+}
+
+function videoStop(id) {
+  const urlSecreta = "https://platzimasseguroquelanasa.com";
+  console.log("Pausamos la url" + urlSecreta);
+}
+
+export class PlatziClass {
+  constructor({
+    name,
+    videoID,
+  }) {
+    this.name = name
+    this.videoID = videoID
+  }
+  reproducir() {
+    videoPlay(this.videoID);
+  }
+  pausar() {
+    videoStop(this.videoID);
+  }
+}
---
+++ src/cap12/module.mjs
@@ -0,0 +1,19 @@
+import {PlatziClass} from "./main.mjs";
+
+const clase67 = new PlatziClass ({
+    name: "JavaScript orientados a objetos",
+    videoID: "wGEA63H6AERGHA6ER1H6",
+});
+
+clase67.reproducir();
+clase67.pausar();
+
+clase67.reproducir();
+clase67.pausar();
+
+
+clase67.reproducir();
+clase67.pausar();
+
+clase67.reproducir();
+clase67.pausar();
